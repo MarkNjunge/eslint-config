9d26c895d04a1abf45cf299a71e5b5a7cd0dfb7b
MarkNjunge <mark.kamau@outlook.com>
2022-02-03 20:26:46 +0300

    Update space-before-function-paren

--- .eslintrc.js
+++ .eslintrc.js
@@ -91,7 +91,10 @@ module.exports = {
     semi: ["error", "always"],
     "semi-style": ["error", "last"],
     "space-before-blocks": "error",
-    "space-before-function-paren": ["error", "never"],
+    "space-before-function-paren": [
+      "error",
+      { anonymous: "always", named: "never", asyncArrow: "always" }
+    ],
     "space-in-parens": ["error", "never"],
     "space-infix-ops": "error",
     "space-unary-ops": ["error", { words: true, nonwords: false }],
